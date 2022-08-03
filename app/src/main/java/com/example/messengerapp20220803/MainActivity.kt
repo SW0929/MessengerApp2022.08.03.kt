52b20c6fd334960df48c64b6044bf0284171f91b
sw <sjugas1234@naver.com>
2022-08-03 16:41:51 +0900

    20220803-01

---
+++ app/src/main/java/com/example/messengerapp20220803/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.messengerapp20220803
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
